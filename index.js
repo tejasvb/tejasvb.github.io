e8883f06e1d2df7028196e8c12be5fee43be2e23
Tejas Vidhyadhar Borkar <63836638+tejasvb@users.noreply.github.com>
2022-11-13 17:51:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('#topheader .navbar-nav a' ).on( 'click', function () {
+	$( '#topheader .navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
+	$( this ).parent( 'li' ).addClass( 'active' );
+}) });
