85a7caf4ecac821f9ddaeab909fc7dfb8722555d
Tejas Vidhyadhar Borkar <63836638+tejasvb@users.noreply.github.com>
2023-04-17 15:14:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ function navbar_on_focus(e){
 	//console.log(this)
 	$(e).addClass("nav-item active");
 }
+functon change_mode(e){
+
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+
+}
