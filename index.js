9fb2106d72657cd4010b583902cbdeee6e9531ae
tejasvb <tvborkar1234@gmail.com>
2024-03-06 23:21:07 +0530

    added night mode

--- index.js
+++ index.js
@@ -4,9 +4,28 @@ function navbar_on_focus(e){
 	//console.log(this)
 	$(e).addClass("nav-item active");
 }
-functon change_mode(e){
+const toggleButton = document.getElementById('toggle');
+const themeStyle = document.getElementById('theme-style');
+const wave = document.querySelector('.parallax');
+const darkmodewave = `  <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(000,000,000,0.7" />
+<use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(000,000,000,0.5)" />
+<use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(000,000,000,0.3)" />
+<use xlink:href="#gentle-wave" x="48" y="7" fill="#000"/>`
+const lightmodewave = ` <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
+<use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+<use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+<use xlink:href="#gentle-wave" x="48" y="7" fill="#fff"/>`
+document.querySelector(".toggle input").addEventListener("change", (evt) => {
+	if (themeStyle.getAttribute('href') === './index.css') {
+        themeStyle.setAttribute('href', './index_dark_mode.css');
+        wave.innerHTML = darkmodewave;
+  
 
-   var element = document.body;
-   element.classList.toggle("dark-mode");
+       
+        
+    } else {
+        themeStyle.setAttribute('href', './index.css');
+        wave.innerHTML = lightmodewave;
+    }
+  });
 
-}
